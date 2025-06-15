bd92548e5d7e6a43a2d8c2c0109b2164024cda14
Jonathan Hiriart <hiriartjonathan@gmail.com>
2025-06-15 19:38:36 -0300

    update

--- SIGU/SIGU.Aplicacion/DTOs/EventoDeportivoDTO.cs
+++ SIGU/SIGU.Aplicacion/DTOs/EventoDeportivoDTO.cs
@@ -1,14 +1,22 @@
+using SIGU.Aplicacion.Entidades;
 using System.ComponentModel.DataAnnotations;
 
 namespace SIGU.Aplicacion.DTOs
 {
 	public class EventoDeportivoDTO
 	{
-		public string Nombre { get; set; } = "";
-		public string Descripcion { get; set; } = "";
-		public DateTime FechaHoraInicio { get; set; }
-		public double DuracionHoras { get; set; }
-		public int CupoMaximo { get; set; }
-		public int ResponsableID { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; } = "";
+		[Required(ErrorMessage = "La descripcion es obligatoria.")]
+        public string Descripcion { get; set; } = "";
+		[Required(ErrorMessage = "La fecha y hora de inicio es obligatoria.")]
+        public DateTime FechaHoraInicio { get; set; }
+		[Required(ErrorMessage = "La duracion en horas es obligatoria.")]
+        public double DuracionHoras { get; set; }
+		[Required(ErrorMessage = "El cupo maximo es obligatorio.")]
+        public int CupoMaximo { get; set; }
+
+		[Required(ErrorMessage = "El responsable es obligatorio")]
+        public Guid ResponsableId{ get; set; }
 	}
 }
