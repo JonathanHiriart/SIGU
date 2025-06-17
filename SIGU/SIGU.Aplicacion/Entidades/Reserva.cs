dd0c64bf175c788e25f8f75c0d7eb942dcccd691
Jonathan Hiriart <hiriartjonathan@gmail.com>
2025-06-17 02:51:14 -0300

    creando RepositorioReserva

--- SIGU/SIGU.Aplicacion/Entidades/Reserva.cs
+++ SIGU/SIGU.Aplicacion/Entidades/Reserva.cs
@@ -1,4 +1,6 @@
 using SIGU.Aplicacion.Enums;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace SIGU.Aplicacion.Entidades;
 public class Reserva
 {
@@ -18,5 +20,14 @@ public class Reserva
         this.usuarioID = usuarioId;
         this.EventoDeportivoId = eventoDeportivoId;
     }
-
+    public void ActualizarDatos(Guid usuarioID, Guid EventoDeportivoId, DateTime FechaAlta, Estado EstadoAsistencia, EventoDeportivo EventoDeportivo, Usuario Usuario)
+    {
+        this.usuarioID = usuarioID;
+        this.EventoDeportivoId = EventoDeportivoId;
+        this.FechaAlta = FechaAlta;
+        this.EstadoAsistencia = EstadoAsistencia;
+        this.Usuario = Usuario;
+        this.EventoDeportivo = EventoDeportivo;
+        this.Usuario = Usuario;
+    }
 }
